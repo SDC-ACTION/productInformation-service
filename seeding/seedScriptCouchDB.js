86119fef2fb468d788765dac744cdfff789509d7
Teddy Kim <teddykim09@gmail.com>
2020-11-24 14:30:34 -0800

    Complete seeding script for couchDB

--- seeding/seedScriptCouchDB.js
+++ seeding/seedScriptCouchDB.js
@@ -2,16 +2,35 @@ const { products } = require('../database/couchdb/models/index.js');
 const { createNewProducts } = require('./seedScript.js');
 
 const insertData = (productsInfo) => {
-  productsInfo.forEach(product => {
-    const insert = products.insert({
+  const savedProducts = [];
 
+  productsInfo.forEach(product => {
+    const insertProduct = products.insert({
+      _id: product.product_id.toString(),
+      description: product.description,
+      title: product.title,
+      brand: product.brand,
+      name: product.category.name,
+      age: product.category.age,
+      player_Count: product.category.playerCount,
+      part_Number: product.specs.part_Number,
+      GTIN: product.specs.GTIN
     })
+    savedProducts.push(insertProduct);
   })
+  return Promise.all(savedProducts);
 };
 
 const seedData = async () => {
-  const allProducts = await createNewProducts();
-  insertData(allProducts);
+  try {
+    const allProducts = await createNewProducts();
+    await insertData(allProducts);
+  } catch(err) {
+    throw new Error(err);
+  }
 }
 
-seedData();
+seedData()
+  .then(() => {
+    process.exit();
+  })
